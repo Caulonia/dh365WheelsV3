e6a874fcc01bded77db9726c8a701f5cafd81acc
Caulonia <89051651+Caulonia@users.noreply.github.com>
2021-11-17 19:19:08 -0300

    Update product.js

--- src/routes/product.js
+++ src/routes/product.js
@@ -4,13 +4,13 @@ let router = express.Router();
 const productsController = require('../controllers/productsController');
 
 router.get('/', productsController.list);
-router.get('/:id', productsController.mostrar);
 router.get('/carrito', productsController.carrito);
 router.get('/detalles', productsController.detalles);
 router.get('/tienda', productsController.store);
 router.get('/crear', productsController.crear);
 router.get('/modificar', productsController.modificar);
 router.get('/eliminar', productsController.eliminar);
+router.get('/:id', productsController.mostrar);
 
 
 module.exports = router;
